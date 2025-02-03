845ab345906dffd4d9da61919f4cc062a26ebd47
ttrotto <armityle@outlook.it>
2025-02-03 15:39:19 -0800

    enable multithreading in tests

--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,5 @@
 mkdir build
 cmake -D CMAKE_BUILD_TYPE=Debug -B build
-cmake --build build
+cmake --build build -j 4
 cd build/tests
 ctest -C Debug
--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,6 @@
 
 mkdir build
 cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -B build
-cmake --build build
+cmake --build build -j 4
 cd build/tests
 ctest -C Debug
